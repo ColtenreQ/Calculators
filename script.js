3e3e68fdb2a436e493559d1a64693d0096a4256a
Coltenw <finalrebellion123@gmail.com>
2023-07-24 22:26:12 -0500

    Fixed some derivative bugs

--- script.js
+++ script.js
@@ -6,14 +6,13 @@
   let calcButton = document.querySelector(".calcButton");
   let calcInput = document.querySelector(".calcInput");
 
-  calcButton.addEventListener('click', () => {
+  calcButton.addEventListener("click", () => {
     let inputString = calcInput.value;
-    let problem = inputString.split('');
-    let solution = solve(problem, derive).join('');
+    let problem = inputString.split("");
+    let solution = solve(problem, derive).join("");
     console.log("This is the solution " + solution);
-    //calcInput.value = solution;
+    calcInput.value = solution;
   });
-
 
   let solve = (array, type) => {
     let temp = [];
@@ -23,8 +22,11 @@
         temp.push(array[i]);
         finalArray.push(type(temp));
         temp = [];
-      } else if (numbers.includes(array[i]) || variables.includes(array[i])
-        || array[i] == "^") {
+      } else if (
+        numbers.includes(array[i]) ||
+        variables.includes(array[i]) ||
+        array[i] == "^"
+      ) {
         temp.push(array[i]);
       } else if (solvers.includes(array[i])) {
         finalArray.push(type(temp));
@@ -33,12 +35,11 @@
       }
     }
     console.log(finalArray);
-    while (solvers.includes(finalArray[finalArray.length -1 ])){
+    while (solvers.includes(finalArray[finalArray.length - 1])) {
       finalArray.splice(finalArray.length - 1);
     }
     return finalArray;
-  }
-
+  };
 
   let derive = (array) => {
     let resolution = [];
@@ -46,65 +47,76 @@
     let tempCoef;
     let hasVariable = false;
     array.forEach((item) => {
-      if(variables.includes(item)) {
+      if (variables.includes(item)) {
         hasVariable = true;
       }
-    })
-    if(hasVariable){
-      for (let i = array.length - 1; i >= 0; i--){
-
+    });
+    if (hasVariable) {
+      for (let i = array.length - 1; i >= 0; i--) {
         if (numbers.includes(array[i]) && array[i - 1] == "^") {
-          tempExp = Number(array.slice(i).join(''));
-          console.log("This is the temp exp " + tempExp);
-          if ( tempExp > 2) {
+          tempExp = Number(array.slice(i).join(""));
+          if (tempExp > 2) {
             resolution.push("^" + String(tempExp - 1));
-            console.log(" This is the exponent ^" + String(tempExp - 1));
           }
-
-        } else if (numbers.includes(array[i]) && variables.includes(array[i + 1])) {
-          tempCoef = Number(array.slice(0, (i + 1)).join(''));
+        } else if (
+          numbers.includes(array[i]) &&
+          variables.includes(array[i + 1])
+        ) {
+          tempCoef = Number(array.slice(0, i + 1).join(""));
+          if (!tempCoef) {
+            tempCoef = 1;
+          }
           tempCoef = tempExp * tempCoef;
           resolution.unshift(String(tempCoef));
-
+        } else if (
+          variables.includes(array[i]) &&
+          array[i + 1] == "^" &&
+          !numbers.includes(array[i - 1])
+        ) {
+          tempCoef = tempExp;
+          resolution.unshift(array[i]);
+          resolution.unshift(tempCoef);
         } else if (variables.includes(array[i])) {
-          if (array[i+1] == "^" && numbers.includes(array[i+2])) {
+          if (array[i + 1] == "^" && numbers.includes(array[i + 2])) {
             resolution.unshift(array[i]);
           }
-        } 
+        }
+        if (solvers.includes(array[i])) {
+          resolution.push("*");
+        }
       }
       console.log("This is the resolution " + resolution);
-      return resolution.join('');
+      return resolution.join("");
     }
-  }
+  };
 }
-
 
 /* Calculator 1 */
 {
-  (function() {
+  (function () {
     let screen = document.querySelector(".screen");
     let buttons = document.querySelectorAll(".btn");
     let clear = document.querySelector(".btn-clear");
     let equal = document.querySelector(".btn-equal");
 
-    buttons.forEach(function(button) {
-      button.addEventListener('click', function(e) {
+    buttons.forEach(function (button) {
+      button.addEventListener("click", function (e) {
         let value = e.target.dataset.num;
         screen.value += value;
-      })
+      });
     });
 
-    equal.addEventListener('click', function(e) {
-      if (screen.value === '') {
-        screen.value = '';
+    equal.addEventListener("click", function (e) {
+      if (screen.value === "") {
+        screen.value = "";
       } else {
         let answer = secureEval(screen.value);
         screen.value = answer;
       }
     });
 
-    clear.addEventListener('click', function(e) {
-      screen.value = '';
+    clear.addEventListener("click", function (e) {
+      screen.value = "";
     });
 
     function secureEval(obj) {
